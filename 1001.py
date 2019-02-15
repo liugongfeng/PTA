c0fe6b480769a53bba8210a3a37ffe351d48a76b
liugongfeng <liugong.feng@aliyun.com>
2019-02-15 21:09:19 +0800

    Update 1001.py

--- 1001.py
+++ 1001.py
@@ -4,3 +4,18 @@ if __name__ == "__main__":
     a , b = int(line[0]), int(line[1])
     
     print('{:,}'.format(a+b))
+    
+   
+  
+    # another way
+    s = input().split(" ")
+    a = int(s[0])
+    b = int(s[1])
+    c = str(a+b)
+    
+    for i in range(len(c)):
+        print(c[i], end="")
+        if c[i] == '-':
+            continue
+        if len(c) - i == 4:
+            print(',', end='')
